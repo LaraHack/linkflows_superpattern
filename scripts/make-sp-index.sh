f027604d6c028a3f83493f4e08c28077fbc16307
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2021-03-18 14:36:46 +0200

    corrected trig and index

--- scripts/make-sp-index.sh
+++ scripts/make-sp-index.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-USAGE="$ scripts/make-fip-index.sh"
+USAGE="$ scripts/make-sp-index.sh"
 
 if [ ! -z $1 ]; then
   echo "Usage: $USAGE"; exit 1
 fi
 
-LASTRELEASE=$(scripts/get-last-release-nr.sh fip)
+LASTRELEASE=$(scripts/get-last-release-nr.sh sp)
 
 if [ -z $LASTRELEASE ]; then
   echo "No previous release found"
   LASTINDEXARG=""
 else
   LASTINDEX=$(
-    cat releases/fip.index.$LASTRELEASE.trig \
+    cat releases/sp.index.$LASTRELEASE.trig \
     | egrep '^@prefix this:' \
     | tail -1 \
     | sed -r 's/.*<(.*)>.*/\1/'
@@ -26,13 +26,11 @@ fi
 
 echo "Making index..."
 scripts/np mkindex \
-  -u https://w3id.org/fair/fip/np/index/ \
-  -c https://orcid.org/0000-0003-2195-3997 \
-  -c https://orcid.org/0000-0001-8888-635X \
+  -u https://w3id.org/linkflows/superpattern/np/index/ \
+  -c https://orcid.org/0000-0002-7114-6459 \
   -c https://orcid.org/0000-0002-1267-0234 \
-  -t "Nanopublications representing the FAIR Implementation Profiles (FIP) Ontology" \
+  -t "Nanopublications representing the SP Superpattern Ontology" \
   -l https://creativecommons.org/publicdomain/zero/1.0/ \
   $LASTINDEXARG \
-  -o fip.index.trig \
-  fip.trig
-
+  -o sp.index.trig \
+  sp.trig
